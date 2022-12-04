3dccb923b87fe15d7770ef31bbde91b893763c95
HassanKamel0 <hassankamel1998@gmail.com>
2022-12-04 10:20:35 +0200

    First commit

---
+++ src/main/java/com/example/demoSecurity/registration/RegistrationController.java
@@ -0,0 +1,23 @@
+package com.example.demoSecurity.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+    @Autowired
+    private RegistrationService registrationService;
+@PostMapping
+    public String register(@RequestBody RegistrationRequest request){
+        return registrationService.register(request);
+    }
+
+    @GetMapping(path = "confirm")
+    public String confirm(@RequestParam("token") String token){
+        return registrationService.confirmToken(token);
+    }
+}
